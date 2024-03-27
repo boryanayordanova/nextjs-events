eed87a6e1a1130a3a3fc8335f4bb300b67e411dd
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2024-03-27 17:21:35 +0200

    done

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,9 +1,20 @@
 import Link from 'next/link';
 import classes from './button.module.css';
 export default function Button(props){
+
+    if(props.link){
+        return (
+            <Link href={props.link} className={classes.btn}>
+                {props.children}
+            </Link>
+        )
+    }
+
     return (
-        <Link href={props.link} className={classes.btn}>
+        <button onClick={props.onClick} className={classes.btn}>
             {props.children}
-        </Link>
+        </button>
     )
+
+
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,21 @@
+import { getAllEvents } from "@/dummy-data";
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 export default function EventsPage() {
+    const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventHandler(year, month){
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
     return (
-        <h1>Events Page</h1>
+        <Fragment>
+            <EventsSearch onSearch={findEventHandler} />
+            <EventList items={events}/>
+        </Fragment>
     )
 }
