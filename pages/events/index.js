6d66cd76af8e3a83ba74a360b40bdb93112cd4ed
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2024-05-06 17:47:30 +0300

    page prerendering and data fetching

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,12 @@
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { Fragment } from "react";
 import { useRouter } from "next/router";
-export default function EventsPage() {
-    const events = getAllEvents();
+import { Fragment } from "react";
+
+export default function EventsPage(props) {
     const router = useRouter();
+    const { events } = props; //const events = props.events();
 
     function findEventHandler(year, month){
         const fullPath = `/events/${year}/${month}`;
@@ -19,3 +20,14 @@ export default function EventsPage() {
         </Fragment>
     )
 }
+
+export async function getStaticProps(){
+    const events = await getAllEvents();
+    return {
+        props: {
+            events: events
+        },
+        revalidate: 60
+    }
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import { getFeaturedEvents } from "@/dummy-data";
+// import { getFeaturedEvents } from "@/dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "@/components/events/event-list";
-export default function HomePage(){
 
-    const featureEvents = getFeaturedEvents();
-
+export default function HomePage(props){
     return (
-        <EventList items={featureEvents}/>
+        <EventList items={props.events}/>
     )
+}
+
+export async function getStaticProps(){
+    const featuredEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events: featuredEvents
+        },
+        revalidate: 1800
+    }
 }
