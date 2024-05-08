0db82030dfa33944428a9ec18af66ea8fb6be65a
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2024-05-08 22:21:26 +0300

    notification context added

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,41 @@
-import { Fragment } from "react";
-import MainHeader from "./main-header";
+// import { Fragment } from "react";
+// import MainHeader from "./main-header";
 
-export default function Layout(props){
-    return <Fragment>
-        <MainHeader />
-        <main>
-            {props.children}
-        </main>
+// export default function Layout(props){
+//     return <Fragment>
+//         <MainHeader />
+//         <main>
+//             {props.children}
+//         </main>
+//     </Fragment>
+// }
+
+import { Fragment, useContext } from 'react';
+
+import MainHeader from './main-header';
+import Notification from '@/components/ui/notification';
+import NotificationContext from '@/store/notification-context';
+
+function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>
+        {props.children}
+        {activeNotification && (
+          <Notification
+            title={activeNotification.title}
+            message={activeNotification.message}
+            status={activeNotification.status}
+          />
+        )}
+      </main>
     </Fragment>
+  );
 }
+
+export default Layout;
